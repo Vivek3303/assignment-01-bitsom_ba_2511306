f67e9bce0bad3ee4b0ef1510b2b5958ceb218464
Vivek3303 <jiovishwakarma799@gmail.com>
2026-03-22 18:06:39 +0530

    Create database schema and insert sample data
    Created tables for Customers, SalesReps, Products, Orders, and OrderItems in 3NF. Inserted sample data for testing.

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,70 @@
+-- Creating Tables in 3NF
 
+CREATE TABLE Customers (
+    customer_id VARCHAR(10) PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    customer_email VARCHAR(100) NOT NULL,
+    customer_city VARCHAR(50)
+);
+
+CREATE TABLE SalesReps (
+    sales_rep_id VARCHAR(10) PRIMARY KEY,
+    sales_rep_name VARCHAR(100) NOT NULL,
+    sales_rep_email VARCHAR(100) NOT NULL,
+    office_address TEXT
+);
+
+CREATE TABLE Products (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50),
+    unit_price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE Orders (
+    order_id VARCHAR(15) PRIMARY KEY,
+    customer_id VARCHAR(10) REFERENCES Customers(customer_id),
+    sales_rep_id VARCHAR(10) REFERENCES SalesReps(sales_rep_id),
+    order_date DATE NOT NULL
+);
+
+CREATE TABLE OrderItems (
+    order_id VARCHAR(15) REFERENCES Orders(order_id),
+    product_id VARCHAR(10) REFERENCES Products(product_id),
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+);
+
+-- Inserting Sample Data
+INSERT INTO Customers (customer_id, customer_name, customer_email, customer_city) VALUES
+('C001', 'Rohan Mehta', 'rohan@gmail.com', 'Mumbai'),
+('C002', 'Priya Sharma', 'priya@gmail.com', 'Delhi'),
+('C003', 'Amit Verma', 'amit@gmail.com', 'Bangalore'),
+('C006', 'Neha Gupta', 'neha@gmail.com', 'Delhi'),
+('C008', 'Kavya Rao', 'kavya@gmail.com', 'Hyderabad');
+
+INSERT INTO SalesReps (sales_rep_id, sales_rep_name, sales_rep_email, office_address) VALUES
+('SR01', 'Deepak Joshi', 'deepak@corp.com', 'Mumbai HQ, Nariman Point, Mumbai - 400021'),
+('SR02', 'Anita Desai', 'anita@corp.com', 'Delhi Office, Connaught Place, New Delhi - 110001'),
+('SR03', 'Ravi Kumar', 'ravi@corp.com', 'South Zone, MG Road, Bangalore - 560001');
+
+INSERT INTO Products (product_id, product_name, category, unit_price) VALUES
+('P001', 'Laptop', 'Electronics', 55000),
+('P002', 'Mouse', 'Electronics', 800),
+('P004', 'Notebook', 'Stationery', 120),
+('P005', 'Headphones', 'Electronics', 3200),
+('P007', 'Pen Set', 'Stationery', 250);
+
+INSERT INTO Orders (order_id, customer_id, sales_rep_id, order_date) VALUES
+('ORD1027', 'C002', 'SR02', '2023-11-02'),
+('ORD1114', 'C001', 'SR01', '2023-08-06'),
+('ORD1153', 'C006', 'SR01', '2023-02-14'),
+('ORD1002', 'C002', 'SR02', '2023-01-17'),
+('ORD1176', 'C001', 'SR01', '2023-03-18');
+
+INSERT INTO OrderItems (order_id, product_id, quantity) VALUES
+('ORD1027', 'P004', 4),
+('ORD1114', 'P007', 2),
+('ORD1153', 'P007', 3),
+('ORD1002', 'P005', 1),
+('ORD1176', 'P002', 3);
